e55684dc0070569f2144c2276a41e8695d79dcc2
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-24 11:33:54 +0700

    mod app add router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import React from "react";
 import "./App.css";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import About from "./pages/About";
+import Users from "./pages/Users";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+      <Router>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand>
+              <Link>Navbar</Link>
+            </Navbar.Brand>
+
+            <Nav className="me-auto">
+              <Nav.Link>
+                <Link to="/">Home</Link>
+              </Nav.Link>
+
+              <Nav.Link>
+                <Link to="/about">About</Link>
+              </Nav.Link>
+
+              <Nav.Link>
+                <Link to="/users">Users</Link>
+              </Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
