fbe20ce11a9819488ab5abf5b76ac8abf5d64839
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-24 16:25:52 +0700

    add offline mode add fetching from api or localstorage

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,9 +1,55 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import { Alert, Container, Table } from "react-bootstrap";
 
-export class Users extends Component {
-  render() {
-    return <div>Users</div>;
-  }
+export default function Users() {
+  const [data, setData] = useState([]);
+  const [mode, setMode] = useState("online");
+
+  useEffect(() => {
+    let apiUrl = "https://jsonplaceholder.typicode.com/users";
+
+    fetch(apiUrl)
+      .then((response) => {
+        response.json().then((result) => {
+          setData(result);
+          localStorage.setItem("users", JSON.stringify(result));
+        });
+      })
+      .catch((e) => {
+        let collection = localStorage.getItem("users");
+        setData(JSON.parse(collection));
+        setMode("offline");
+      });
+  }, []);
+
+  return (
+    <Container className="pt-5">
+      {mode === "offline" ? (
+        <Alert variant="danger">
+          You are in offline mode, please check your internet connections
+        </Alert>
+      ) : null}
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.address.street}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  );
 }
-
-export default Users;
