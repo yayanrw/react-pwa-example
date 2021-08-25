1fcd82f902e5ad57acde7fc31d7e61f9b04394b7
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-25 10:49:40 +0700

    add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+var firebaseConfig = {
+  apiKey: "AIzaSyBISrIBXJuSLkdHrj3VcoqL_f59WmElaOg",
+  authDomain: "react-pwa-example-22ecf.firebaseapp.com",
+  projectId: "react-pwa-example-22ecf",
+  storageBucket: "react-pwa-example-22ecf.appspot.com",
+  messagingSenderId: "714433780933",
+  appId: "1:714433780933:web:0cab5e1fe83c9032e3a4ac",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export default firebase;
