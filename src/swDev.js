27a49e369e43dbf60b82f91a932bdebc3dcfca98
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-25 09:24:57 +0700

    add config webpush

--- src/swDev.js
+++ src/swDev.js
@@ -1,9 +1,36 @@
+const webpush = require("web-push");
+
 export default function swDef() {
+  function urlBase64ToUint8Array(base64String) {
+    var padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+    var base64 = (base64String + padding)
+      .replace(/\-/g, "+")
+      .replace(/_/g, "/");
+
+    var rawData = window.atob(base64);
+    var outputArray = new Uint8Array(rawData.length);
+
+    for (var i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+  }
+
+  function determineAppServerKey() {
+    const vapidKeys = webpush.generateVAPIDKeys();
+    return urlBase64ToUint8Array(vapidKeys.publicKey);
+  }
+
   let swUrl = `${process.env.PUBLIC_URL}/sw.js`;
   navigator.serviceWorker
     .register(swUrl)
     .then((response) => {
-      console.warn("response: ", response);
+      return response.pushManager.getSubscription().then(function (subs) {
+        return response.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: determineAppServerKey(),
+        });
+      });
     })
     .catch((e) => {
       console.error("error: ", e);
